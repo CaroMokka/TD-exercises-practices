70764e598a594ca1585855120264a09b3ef7e480
carolina <ci.arayagonzalez@gmail.com>
2024-08-17 13:21:50 -0400

    ft- add filter

--- practices/M3-practice4/assets/js/script.js
+++ practices/M3-practice4/assets/js/script.js
@@ -1,8 +1,8 @@
-//console.log(superHeroes)
+console.log(superHeroes)
 
 function listarSuperheroes(listado) {
     console.log(listado)
-    //$("#list-superheroes").html("")
+    $("#list-superheroes tbody").html("")
     for(const item of listado){
         $("#list-superheroes tbody").append(`
             <tr>
@@ -16,7 +16,7 @@ function listarSuperheroes(listado) {
                     Acciones
                 </button>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Eliminar</a></li>
+                    <li><a class="dropdown-item button-delete" href="#" data-id="${item.id}">Eliminar</a></li>
                 </ul>
                 </td>
             </tr>
@@ -28,8 +28,32 @@ listarSuperheroes(superHeroes)
 
 $(function(){//Esta funcion ayuda a que todo el html se cargue primero
     //accion de eliminar un elemento
+    $(document).on("click", ".button-delete", function(event) {
+        event.preventDefault();
+        var idEliminar = $(this).data("id")
+
+        if(confirm("¿Seguro desea eliminar este super héroe?")) {
+            // Ejemplo 1 para eliminar
+            // var indice = superHeroes.findIndex(item => item.id == idEliminar)
+            // superHeroes.splice(indice)
+
+            // Ejemplo 2 para eliminar
+            superHeroes = superHeroes.filter(item => item.id != idEliminar)
+
+            listarSuperheroes(superHeroes)
+        }
+    })
 
     //Mostrar o ocultar filtro
+    $("#show-hide").click(function() {
+        if($("#filter").hasClass("d-none")) { // Lógica para mostrar
+            $("#filter").removeClass("d-none")
+            $("#show-hide").removeClass("btn-success").addClass("btn-danger").html("Ocultar")
+        } else { // Lógica para ocultar
+            $("#filter").addClass("d-none")
+            $("#show-hide").removeClass("btn-danger").addClass("btn-success").html("Mostrar")
+        }
+    })
 
     //
 })
