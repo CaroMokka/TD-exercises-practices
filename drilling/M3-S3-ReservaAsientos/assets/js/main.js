7959713a4dac809a0f2174ddae005368b32422fc
carolina <ci.arayagonzalez@gmail.com>
2024-08-22 21:58:39 -0400

    agrega nueva estructura html

--- drilling/M3-S3-ReservaAsientos/assets/js/main.js
+++ drilling/M3-S3-ReservaAsientos/assets/js/main.js
@@ -1,33 +1,68 @@
 $(function () {
-  do {
-    var tipoAsiento = prompt(
-      "Seleccione el tipo de asiento: \n 1. Asiento Normal. Valor $5.000 \n 2. Asiento Preferencial. Valor $9.000 \n 3. Asiento VIP. Valor $13.000"
-    );
-    if (isNaN(tipoAsiento)) {
-      alert("Por favor ingresar un valor numérico");
-    }
-    if (tipoAsiento < 1) {
-      alert("Por favor ingresar un número positivo");
-    }
-    if (tipoAsiento > 3) {
-      alert("La opción ingresada no es válida ");
-    }
-  } while (isNaN(tipoAsiento) || tipoAsiento < 1 || tipoAsiento > 3);
 
-  do {
-    var userQuantitySelected = prompt(
-      "Ingrese la cantidad de asientos que desea reservar:"
-    );
-    if (isNaN(userQuantitySelected)) {
-      alert("Por favor ingresar un valor numérico");
-    }
-  } while (isNaN(userQuantitySelected));
 
-  if (userQuantitySelected >= 5) {
-    alert("Por favor ingresar una cantidad permitida de maximo de reserva");
-  }
 
-  function optDcto() {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  // do {
+  //   var tipoAsiento = prompt(
+  //     "Seleccione el tipo de asiento: \n 1. Asiento Normal. Valor $5.000 \n 2. Asiento Preferencial. Valor $9.000 \n 3. Asiento VIP. Valor $13.000"
+  //   );
+  //   if (isNaN(tipoAsiento)) {
+  //     alert("Por favor ingresar un valor numérico");
+  //   }
+  //   if (tipoAsiento < 1) {
+  //     alert("Por favor ingresar un número positivo");
+  //   }
+  //   if (tipoAsiento > 3) {
+  //     alert("La opción ingresada no es válida ");
+  //   }
+  // } while (isNaN(tipoAsiento) || tipoAsiento < 1 || tipoAsiento > 3);
+
+  // do {
+  //   var userQuantitySelected = prompt(
+  //     "Ingrese la cantidad de asientos que desea reservar:"
+  //   );
+  //   if (isNaN(userQuantitySelected)) {
+  //     alert("Por favor ingresar un valor numérico");
+  //   }
+  // } while (isNaN(userQuantitySelected));
+
+  // if (userQuantitySelected >= 5) {
+  //   alert("Por favor ingresar una cantidad permitida de maximo de reserva");
+  // }
+
+  // function optDcto() {}
 });
 //asientos reservados es mayor o igual a 5, se debe aplicar un descuento del 10%
 // es igual o menor a 0 se le debe indicar al usuario que debe ingresar un número positivo y volver a solicitarle la cantidad.
