33db635300db709646ee380ed11bd22b6e06184a
carolina <ci.arayagonzalez@gmail.com>
2024-08-27 16:21:42 -0400

    add conditional fly origin and destination

--- rebounce/M3-S4-FlyNow/fly_now/script.js
+++ rebounce/M3-S4-FlyNow/fly_now/script.js
@@ -9,20 +9,15 @@ $(function () {
     let destino_ciudad = $("#to").val();
     console.log(origen_ciudad, destino_ciudad);
 
+    mostrarEscalas(origen_ciudad, destino_ciudad);
+
     if (
       (origen_ciudad == "Chicago" && destino_ciudad == "Venice") ||
       (origen_ciudad == "Boston" && destino_ciudad == "Paris")
     ) {
-      return console.log("Esta ruta tiene escalas");
+      return $("#trans").html("Ojo! Tu vuelo tiene una escala");
     } else {
-      return console.log("No tiene escalas");
+      return $("#trans").html("Tu vuelo no tiene una escala");
     }
-
   });
 });
-
-//condicion de comparacion entre ciudad de origen y de destino
-//segun resultado me dara mensaje que sera inyectado dentro de html
-
-//chicago + venice
-//boston + paris
