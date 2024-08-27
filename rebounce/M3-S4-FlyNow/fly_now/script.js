651e00afd84f635d582a427d0322d493f2a28c1b
carolina <ci.arayagonzalez@gmail.com>
2024-08-27 16:20:08 -0400

    add condition fly origin - destination

--- rebounce/M3-S4-FlyNow/fly_now/script.js
+++ rebounce/M3-S4-FlyNow/fly_now/script.js
@@ -1,3 +1,28 @@
-$(function(){
-  
-})
+$(function () {
+  function mostrarEscalas(origenCiudad, destinoCiudad) {
+    $("#origin").html(origenCiudad);
+    $("#dest").html(destinoCiudad);
+  }
+
+  $("#btn-vuelo").on("click", function () {
+    let origen_ciudad = $("#from").val();
+    let destino_ciudad = $("#to").val();
+    console.log(origen_ciudad, destino_ciudad);
+
+    if (
+      (origen_ciudad == "Chicago" && destino_ciudad == "Venice") ||
+      (origen_ciudad == "Boston" && destino_ciudad == "Paris")
+    ) {
+      return console.log("Esta ruta tiene escalas");
+    } else {
+      return console.log("No tiene escalas");
+    }
+
+  });
+});
+
+//condicion de comparacion entre ciudad de origen y de destino
+//segun resultado me dara mensaje que sera inyectado dentro de html
+
+//chicago + venice
+//boston + paris
