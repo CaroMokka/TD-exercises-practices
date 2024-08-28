05378c7d93d34a0d5ab1fd8eb8c43be64c53b65e
carolina <ci.arayagonzalez@gmail.com>
2024-08-28 16:29:22 -0400

    add card-form

---
+++ rebounce/M3-S5-SafeMe/script.js
@@ -0,0 +1,17 @@
+$(function(){
+    let nombre
+    let correo
+    let trabajo
+    let telefono
+    let sobre_mi
+
+    function Usuario(nombre, correo, trabajo, telefono, sobreMi) {
+        this.nombre = nombre
+        this.correo = correo
+        this.trabajo = trabajo
+        this.telefono =telefono
+        this.sobre_mi = sobreMi
+    }
+
+    //un evento clik con btn "enviar"
+})
