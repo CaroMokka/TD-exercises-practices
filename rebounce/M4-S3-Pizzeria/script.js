d00dbf5c1f3a765a1153ad6e5cd5885415d40265
carolina <ci.arayagonzalez@gmail.com>
2024-09-01 22:11:40 -0400

    fix checkbox list

--- rebounce/M4-S3-Pizzeria/script.js
+++ rebounce/M4-S3-Pizzeria/script.js
@@ -9,86 +9,78 @@ $(() => {
     "Piña",
     "Pimenton",
   ];
-  
-  const mostrarListaIngredientes = () => {
-    for(const item of ingredientes ) {
-        $("#ingredientes").append(`
-        <input class="form-check-input check-ingrediente" type="checkbox" value=${item} id="flexCheckDefault">${item}</input>
-        `);
-      }
-  }
-  mostrarListaIngredientes()
 
-  let seleccionados = []
+  const mostrarListaIngredientes = () => {
+    for (const item of ingredientes) {
+      $("#ingredientes").append(`
+        <div class="d-flex" >
+            <input class="form-check-input me-2 check-ingrediente" type="checkbox" value=${item} id="flexCheckDefault"></input>
+            <label for="flexCheckDefault" class="form-label d-flex">${item}</label>
+        </div>
+        `);
+    }
+  };
+  mostrarListaIngredientes();
+
+  let seleccionados = [];
 
   const resumenDeCompra = (totalValorExtra) => {
-      let displayValoresCompra = $("#valorExtra")
-      //console.log(typeof totalValorExtra)
-      displayValoresCompra.html(`$ ${totalValorExtra}`)
-      pagoValorFinal(totalValorExtra)
-  }
+    let displayValoresCompra = $("#valorExtra");
+    displayValoresCompra.html(`$ ${totalValorExtra}`);
+  };
 
-  const pagoValorFinal = (totalValorExtra) => {
-      console.log("totalvalorextra", totalValorExtra)
-      let valorPizzaXL = $("#valor-pizzaxl").data("pizza")
-      let valorExtra = totalValorExtra
-    //   let valorPropina = parseInt($("#propina").val())
-    //   $("#valor-propina").html(`$ ${valorPropina}`)
-    $(document).on("change", "#propina", (e) => {
-        e.preventDefault()
-        let valorPropina = e.target.value
-        console.log(valorPropina)
-        $("#valor-propina").html(`$ ${valorPropina}`)
-    })
-      console.log("valores de resumen", valorPizzaXL, valorExtra)
+  $(document).on("change", "#propina", (e) => {
+    e.preventDefault();
+    let valorPropina = e.target.value;
+    if (isNaN(valorPropina)) {
+      $("#valor-propina").html(`$ ${valorPropina}`);
+    } else {
+      $("#valor-propina").html(`$ ${0}`);
+    }
+  });
 
-  }
+  $(document).on("click", "#btn-enviar", () => {
+    let valorPropinaFinal = parseInt($("#propina").val());
+    console.log(typeof valorPropinaFinal, valorPropinaFinal);
+    if (valorPropinaFinal == 0 || isNaN(valorPropinaFinal)) {
+      alert("Aun no ha definido una propina");
+    } else {
+      let valorPropina = $("#propina").val();
+      $("#valor-propina").html(`$ ${valorPropina}`);
+      alert(`Su propina de $ ${valorPropinaFinal} ha sido enviada`);
+    }
+  });
 
   const mostrarIngredientesExtras = (listaIngredientesExtras) => {
-      $("#extra-seleccionados").html("")
-      let totalValorExtra = 0
-    for(let ingredienteExtra of listaIngredientesExtras) {
-        $("#extra-seleccionados").append(`
+    $("#extra-seleccionados").html("");
+    let totalValorExtra = 0;
+    for (let ingredienteExtra of listaIngredientesExtras) {
+      $("#extra-seleccionados").append(`
             <p class="fs-5" text-end>${ingredienteExtra}</p>
-        `)
-        totalValorExtra += 800
-
+        `);
+      totalValorExtra += 800;
     }
-    resumenDeCompra(totalValorExtra)
-    console.log("lista exxtras", listaIngredientesExtras)
-    console.log("valor final extra", totalValorExtra)
-  }
+    resumenDeCompra(totalValorExtra);
+    console.log("lista exxtras", listaIngredientesExtras);
+    console.log("valor final extra", totalValorExtra);
+  };
 
   const mostrarIngredientesSeleccionados = (listaSeleccionados) => {
-    //console.log("lista seleccionando", listaSeleccionados)
-    $("#seleccionados").html("")
-    for(const seleccion of listaSeleccionados) {
-        $("#seleccionados").append(`
+    $("#seleccionados").html("");
+    for (const seleccion of listaSeleccionados) {
+      $("#seleccionados").append(`
             <p class="fs-5" >${seleccion}</p>
-        `)
+        `);
     }
-    if(listaSeleccionados.length > 3) {
-        let listaIngredientesExtras = listaSeleccionados.slice(3)
-        //console.log(listaIngredientesExtras)
-        mostrarIngredientesExtras(listaIngredientesExtras)
-        
+    if (listaSeleccionados.length > 3) {
+      let listaIngredientesExtras = listaSeleccionados.slice(3);
+      mostrarIngredientesExtras(listaIngredientesExtras);
     }
-  }
- 
+  };
 
-  $(document).on("click", ".check-ingrediente", function(){
-      //revisar bine la diferencia entre evento channge y click para este problema
-    const checkeado = $(this).prop("checked")
-    //console.log("checkeado", checkeado)
-    const valor = $(this).val()
-    //console.log("valor", valor)
-    seleccionados.push(valor)
-    //console.log(seleccionados)
-    mostrarIngredientesSeleccionados(seleccionados)
-    //si esta chequeado guardalo dentro de un array de seleccioandos
-    //sino
-    //
-  })
-
-
+  $(document).on("click", ".check-ingrediente", function () {
+    const valor = $(this).val();
+    seleccionados.push(valor);
+    mostrarIngredientesSeleccionados(seleccionados);
+  });
 });
