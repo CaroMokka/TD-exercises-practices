81df0452971e4cc0e1a010a58b23dff0ec0657d4
carolina <ci.arayagonzalez@gmail.com>
2024-09-16 16:35:53 -0300

    agrega relojes con intervalo

--- rebounce/M4-S6-HorasDelMundo/main.js
+++ rebounce/M4-S6-HorasDelMundo/main.js
@@ -1,148 +1,33 @@
+import ciudades from "./data.js";
+
 $(() => {
-  const opcionesCiudad = [
-    {
-      name: "Santiago de Chile",
-      timeZone: "America/Santiago",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    },
-    {
-      name: "París",
-      timeZone: "Europe/Paris",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    },
-    {
-      name: "Londres",
-      timeZone: "Europe/London",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    },
-    {
-      name: "Nueva York",
-      timeZone: "America/New_York",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    },
-    {
-      name: "San Petersburgo",
-      timeZone: "Europe/Moscow",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    },
-    {
-      name: "Beijing",
-      timeZone: "Asia/Shanghai",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    },
-    {
-      name: "Seúl",
-      timeZone: "Asia/Seoul",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    },
-  ];
+  function mostrarHoraCiudad(ciudad) {
+    horaEnDirecto(ciudad);
+    //setInterval(() => horaEnDirecto(ciudad), 1000)
+  }
 
-  const mostrarHora = async () => {
-    $("#relojes").html("");
-    for (const opcion of opcionesCiudad) {
-      const horaCiudad = new Intl.DateTimeFormat("es-CL", opcion).format(
-        new Date()
-      );
-        const reloj = await mostrarRelojes(opcion.name, horaCiudad)
-        console.log(reloj)
-    //  mostrarRelojes(opcion.name,horaCiudad).then(res => {
-    //      console.log(res)
-    //  }).catch(error => console.log(error))
-    }
-  };
-  //setInterval(mostrarHora, 1000);
+  function horaEnDirecto(ciudad) {
+    const hora_ciudad = new Intl.DateTimeFormat("es-CL", ciudad).format(
+      new Date()
+    );
+    mostrarRelojes(ciudad.name, hora_ciudad);
+    
+    console.log(`Hora Actual: ${hora_ciudad} y Ciudad: ${ciudad.name}`);
+    return hora_ciudad; //hora estatica
+  }
 
-  const delay = (duration) => {
-    return new Promise((res, rej) => {
-        setTimeout(()=>{
-            res()
-        }, duration * 1_000)
-    })
-}
+  function mostrarRelojes(nombreCiudad, horaCiudad) {
+    $("#relojes").html();
 
-  const mostrarRelojes = (ciudad, hora) => {
-    //let display = $("#relojes");
-    return new Promise( async (res, rej)=> {
-        if(ciudad && hora){
-            await delay(4)
-            res(`Ciudad ${ciudad}, Hora ${hora}`)
-        } else {
-            rej("No existe reloj")
-        }
-         
-            
-    })
-    
-    
-    
-    
-  };
+    $("#relojes").append(`
+      <p>${horaCiudad}</p>
+        <p>${nombreCiudad}</p>
+      `);
+  }
+
+  for (let i = 0; i < ciudades.length; i++) {
+    setTimeout(() => {
+      mostrarHoraCiudad(ciudades[i]);
+    }, i * 4_000);
+  }
 });
-
-
-// display.append(`
-//             <div class="card mb-3" style="max-width: 20rem;">
-//                         <div class="row g-0">
-//                           <div class="col-md-4">
-//                             <img src="..." class="img-fluid rounded-start" alt="imagen-bandera">
-//                           </div>
-//                           <div class="col-md-8">
-//                             <div class="card-body">
-//                               <h5 class="card-title">${hora}</h5>
-//                               <h4 class="card-text">${reloj.name}</h4>
-//                             </div>
-//                           </div>
-//                         </div>
-//           </div>
-//             `)
-
-
-
-
-// const delay = (duration) => {
-//     return new Promise((res, rej) => {
-//         setTimeout(()=>{
-//             res()
-//         }, duration * 1_000)
-//     })
-// }
-
-//   const mostrarRelojes = (hora, ciudad) => {
-//     let display = $("#relojes");
-//     display.append(`
-//             <div class="card mb-3" style="max-width: 20rem;">
-//                         <div class="row g-0">
-//                           <div class="col-md-4">
-//                             <img src="..." class="img-fluid rounded-start" alt="imagen-bandera">
-//                           </div>
-//                           <div class="col-md-8">
-//                             <div class="card-body">
-//                               <h5 class="card-title">${hora}</h5>
-//                               <h4 class="card-text">${ciudad}</h4>
-//                             </div>
-//                           </div>
-//                         </div>
-//           </div>
-//             `)
-   
-    
-    
-//   };
-
-
-
-
--- rebounce/M4-S6-HorasDelMundo/script.js
+++ rebounce/M4-S6-HorasDelMundo/script.js
@@ -1,43 +1,88 @@
-// const cuidades = [
-//     "santiago", "paris", "tokyo"
-// ]
+// $(() => {
+//   function actualizarHoraCiudad(ciudad) {
+//     $("#relojes").html("");
+//     const horaCiudad = new Intl.DateTimeFormat("es-CL", ciudad).format(
+//       new Date()
+//     );
+//     $("#relojes").append(`
+//     <div>${horaCiudad}</div>
+//     <div>${ciudad.name}</div>
+// `);
+//     }
+//   function delay(ms) {
+//     return new Promise((resolve) => setTimeout(resolve, ms));
+//   }
 
+//   async function mostrarHoraCiudad(ciudad) {
+//     actualizarHoraCiudad(ciudad)
+//     setInterval(() => {
+//       actualizarHoraCiudad(ciudad);
+//     }, 1_000);
+//   }
 
-// const peticionAsincrona = () => {
-//     return new Promise((res) => {
-//         setTimeout(() => {
-//             res(console.log("Peticion Asincrona"))
-//         }, 2_000);
-//     })
-    
-// }
-
-//funciones
-const mostrarHoraSantiago = async () => {
-    return new Promise((resolve) => {
-        resolve(
-            resolve(
-                setTimeout(() => {
-                    console.log("Santiago")
-                }, 4_000)
-            )
-        )
-    })
-}
-const mostrarHoraParis = async () => {
-    return new Promise((resolve) => {
-        resolve(
-            resolve(
-                setTimeout(() => {
-                    console.log("Paris")
-                }, 8_000)
-            )
-        )
-    })
-}
-
-mostrarHoraSantiago()
-mostrarHoraParis()
-
-
-
+//   async function inicioRelojMundial() {
+//     const ciudades = [
+//       {
+//         name: "Santiago de Chile",
+//         delay: 0,
+//         timeZone: "America/Santiago",
+//         hour: "2-digit",
+//         minute: "2-digit",
+//         second: "2-digit",
+//       },
+//       {
+//         name: "París",
+//         delay: 4_000,
+//         timeZone: "Europe/Paris",
+//         hour: "2-digit",
+//         minute: "2-digit",
+//         second: "2-digit",
+//       },
+//       {
+//         name: "Londres",
+//         delay: 8_000,
+//         timeZone: "Europe/London",
+//         hour: "2-digit",
+//         minute: "2-digit",
+//         second: "2-digit",
+//       },
+//       {
+//         name: "Nueva York",
+//         delay: 12_000,
+//         timeZone: "America/New_York",
+//         hour: "2-digit",
+//         minute: "2-digit",
+//         second: "2-digit",
+//       },
+//       {
+//         name: "San Petersburgo",
+//         delay: 16_000,
+//         timeZone: "Europe/Moscow",
+//         hour: "2-digit",
+//         minute: "2-digit",
+//         second: "2-digit",
+//       },
+//       {
+//         name: "Beijing",
+//         delay: 20_000,
+//         timeZone: "Asia/Shanghai",
+//         hour: "2-digit",
+//         minute: "2-digit",
+//         second: "2-digit",
+//       },
+//       {
+//         name: "Seúl",
+//         delay: 24_000,
+//         timeZone: "Asia/Seoul",
+//         hour: "2-digit",
+//         minute: "2-digit",
+//         second: "2-digit",
+//       },
+//     ];
+//     for (const ciudad of ciudades) {
+//       await delay(ciudad.delay);
+//       mostrarHoraCiudad(ciudad);
+//     }
+//   }
+  //inicioRelojMundial();
+//});
