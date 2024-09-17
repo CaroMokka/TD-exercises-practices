f4d078c35682fb0a1c4e734f255d792164f213f2
carolina <ci.arayagonzalez@gmail.com>
2024-09-16 22:18:34 -0300

    refactor form add tasks

--- drilling/M3-S6-GestorTareas/script.js
+++ drilling/M3-S6-GestorTareas/script.js
@@ -3,30 +3,22 @@ class Task {
     this.name_task = name;
     this.state_task = state;
   }
-  
-  
-    
 }
 
-function isActive(list){
-    $("#task_state").click(()=>{
-        console.log(!this.state_task)
-        //si es true entonces cambia a completada
-        if(!this.state_task){
-            $("#status_task").html("Completada")
-            $("#status_task").addClass("bg-success bg-opacity-25")
-            const listFiltered = list.filter((task) => {
-                task.state_task === "Completada"
-                
-            })
-     
-            
-        }
-        console.log("Lista filtrada", listFiltered)
-        
-        
-    })
-  }
+function isActive(list) {
+  $(".completed").click(() => {
+    console.log(!this.state_task);
+    //si es true entonces cambia a completada
+    if (!this.state_task) {
+      $(".completed").html("Completada");
+      $(".completed").addClass("bg-success bg-opacity-25");
+      const listFiltered = list.filter((task) => {
+        task.state_task === "Completada";
+      });
+    }
+    console.log("Lista filtrada", listFiltered);
+  });
+}
 
 // function filterTasks(list) {
 //     const listFiltered = list.filter((task) => {
@@ -34,15 +26,15 @@ function isActive(list){
 //     })
 //     console.log("Lista filtrada", listFiltered)
 // }
-function quantityTasks(list){
-    //$("#total_tasks").html("")
-      let suma = 0
-      for(let i = 0; i < list.length; i++){
-        suma += 1
-      }
-      $("#total_tasks").html(`Cantidad de tareas: ${suma}`)
-      return suma
+function quantityTasks(list) {
+  //$("#total_tasks").html("")
+  let suma = 0;
+  for (let i = 0; i < list.length; i++) {
+    suma += 1;
   }
+  $("#total_tasks").html(`Cantidad de tareas: ${suma}`);
+  return suma;
+}
 
 $(() => {
   let listado = [];
@@ -51,36 +43,39 @@ $(() => {
     $("#display_tasks").html("");
     for (const task of listado) {
       $("#display_tasks").append(`
-            <div class="d-flex">
-                <input class="form-check-input me-2 checked" type="checkbox" id="task_state"></input>
-                <label for="flexCheckDefault" class="form-label d-flex">${
-                  task.name_task
-                }</label>
-                <span id="status_task" class="justify-content-end" >${
+            <div class="col-12 d-flex">
+                <div class="col-9 d-flex" >
+                  <input class="form-check-input me-2 checked" type="checkbox" id="task_state"></input>
+                  <label for="flexCheckDefault" class="form-label d-flex">${
+                    task.name_task
+                  }</label>
+                </div>
+             
+                  <div class="col-3 text-center fw-bold completed">${
                     task.state_task ? "" : "Pendiente"
-                  }</span>
+                  }</div>
+           
             </div>
         `);
     }
-    
   }
-
-
-
-
 
   $("#form").submit(function (e) {
     e.preventDefault();
-    //$("#add_task").val("")
 
-    let descriptionTask = $("#add_task").val();
+    let nameTask = $("#add_task").val();
+    let statusTask = $("#add_status").val()
+    console.log("status", statusTask)
 
-    const task = new Task(descriptionTask, false);
+    if (nameTask == "") {
+      return alert("Ingrese una tarea");
+    }
+
+    const task = new Task(nameTask, false);
     listado.push(task);
     listTasks(listado);
-    isActive(listado)
+    //isActive(listado)
     //filterTasks(listado)
-    quantityTasks(listado)
-   
+    //quantityTasks(listado)
   });
 });
