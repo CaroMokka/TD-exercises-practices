f060b47dcce5f3b8e46f8694d7d02835307d3f45
carolina <ci.arayagonzalez@gmail.com>
2024-09-17 22:01:17 -0300

    fix-quantity-tasks

--- drilling/M3-S6-GestorTareas/script.js
+++ drilling/M3-S6-GestorTareas/script.js
@@ -5,33 +5,31 @@ class Task {
   }
 }
 
-function isActive(list) {
-  $(".completed").click(() => {
-    console.log(!this.state_task);
-    //si es true entonces cambia a completada
-    if (!this.state_task) {
-      $(".completed").html("Completada");
-      $(".completed").addClass("bg-success bg-opacity-25");
-      const listFiltered = list.filter((task) => {
-        task.state_task === "Completada";
-      });
-    }
-    console.log("Lista filtrada", listFiltered);
-  });
-}
-
-function quantityTasks(list) {
-  //$("#total_tasks").html("")
-  let suma = 0;
-  for (let i = 0; i < list.length; i++) {
-    suma += 1;
-  }
-  $("#total_tasks").html(`Cantidad de tareas: ${suma}`);
-  return suma;
-}
-
 $(() => {
   let listado = [];
+  let btnTaskDone = $("#btn-success")
+  let btnTaskPending = $("#btn-danger")
+
+
+  btnTaskDone.on("click", function(){
+    listado.forEach((task, index) => {
+      if(task.state_task){
+        console.log("tarea confirmada")
+        $(".pending-task").ClassAdd("d-none")
+      } 
+
+    })
+  })
+  
+
+  function quantityTasks(list) {
+    //$("#total_tasks").html("")
+    let suma = 1;
+    for (let i = 0; i < list.length; i++) {
+      suma = suma + 1;
+    }
+    $("#total_tasks").html(`Cantidad de tareas: ${suma}`);
+  }
 
   function listTasks(listado) {
     $("#display_tasks").html("");
@@ -48,7 +46,7 @@ $(() => {
         `);
       } else {
         $("#display_tasks").append(`
-            <div class="col-12 d-flex">
+            <div class="col-12 d-flex pending-task">
                 <div class="col-9 d-flex" >
                   <input class="form-check-input me-2 checked" type="checkbox" id="task_state"></input>
                   <label for="flexCheckDefault" class="form-label d-flex">${task.name_task}</label>
@@ -60,14 +58,33 @@ $(() => {
     }
   }
 
+  // function filterTasksStatus(listado) {
+  //   for(const task of listado){
+  //     console.log(task.state_task)
+  //     if(task.state_task){
+  //       console.log("CONFIRMADO")
+  //     } else {
+  //       console.log("PENDIENTE")
+  //     }
+  //   }
+  // }
+  // filterTasksStatus(listado)
+
   $("#form").submit(function (e) {
     e.preventDefault();
 
     let nameTask = $("#add_task").val();
+    $("#add_task").val("");
     let statusTask = document.getElementById("add_status");
+
+    quantityTasks(listado);
+    
+    console.log("LISTA",listado)
+
     if (nameTask == "") {
       return alert("Ingrese una tarea");
     }
+    
     if (statusTask.checked) {
       console.log("Check esta marcado");
       const task = new Task(nameTask, true);
@@ -81,11 +98,7 @@ $(() => {
       listTasks(listado);
       return task;
     }
-
-    //isActive(listado)
-    //filterTasks(listado)
-    //quantityTasks(listado)
   });
+
+
 });
-
-
