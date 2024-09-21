59a7387b5b04674d3ae0fd4c0436395d5758a08d
carolina <ci.arayagonzalez@gmail.com>
2024-09-20 22:16:24 -0300

    add book list and constructor book

--- drilling/M3-S7-GestorLibros/data.js
+++ drilling/M3-S7-GestorLibros/data.js
@@ -3,7 +3,38 @@ const books = [
         title: "El poder del ahora",
         autor: "Eckhart Tolle",
         date_published: "2012"
+    },
+    {
+        title: "Arte y percepción visual: Psicología del ojo creador",
+        autor: "Rudolf Arnheim",
+        date_published: "1954"
+    },
+    {
+        title: "¿Qué estás mirando?",
+        autor: "Will Gompertz",
+        date_published: "2012"
+    },
+    {
+        title: "Sintaxis de la imagen",
+        autor: "Donis A. Dondis",
+        date_published: "1973"
+    },
+    {
+        title: "Historia de la estética",
+        autor: "Raymond Bayer",
+        date_published: "1961"
+    },
+    {
+        title: "Esculpir en el tiempo",
+        autor: "Andréi Tarkovski",
+        date_published: "1984"
+    },
+    {
+        title: "El hombre y sus símbolos",
+        autor: "Carl G. Jung",
+        date_published: "1964"
     }
+
 ]
 
 export default books
--- drilling/M3-S7-GestorLibros/script.js
+++ drilling/M3-S7-GestorLibros/script.js
@@ -1,19 +1,23 @@
-//agregar y editar innformaciuon sobre los libbros
-//buscador de libros por diferentes critewrios
-//funiones que permitar gestionar y visualizar la indormacion de los libbros
-//buscar y filtrar libros por nombre de autor
-//Año publicación mostrar mensaje cuando los datos no exista (No especificado)
 import books from "./data.js"
 class Book {
     constructor(title, autor, year){
         this.title_book  = title
         this.autor_book = autor
-        this.yearPublished = year
-    }
-    //funciones
-    editBooks() {
-        //Aqui editar conn metodo de String (agregar _ entre espacios u otra cosa)
-    }
-    showBooks(){}
+        this.year_published = year
+    }    
 }
-console.log("Libros", books)
+window.onload = () => {
+    let newbooks = []
+    const showFormatedBook = () => {
+        books.map((item) => {
+            for(const clave in item ){
+                item[clave] = item[clave].replaceAll("."," ").replaceAll(" ", "_").toLowerCase()           
+            }
+            const book = new Book(item.title, item.autor, item.date_published)
+            newbooks.push(book)
+        } )
+    }
+    showFormatedBook() 
+    console.log("Nuevos libros", newbooks)
+}
+
