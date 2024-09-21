f36b846d75cdb0825bd75ed6ae7e738aaa72d4a4
carolina <ci.arayagonzalez@gmail.com>
2024-09-21 19:18:52 -0300

    agrega form y funciones de filtrar

--- drilling/M3-S7-GestorLibros/script.js
+++ drilling/M3-S7-GestorLibros/script.js
@@ -1,25 +1,66 @@
-import books from "./data.js"
+import books from "./data.js";
 class Book {
-    constructor(title, autor, year){
-        this.title_book  = title
-        this.autor_book = autor
-        this.year_published = year
-    }    
+  constructor(title, autor, year) {
+    this.title_book = title;
+    this.autor_book = autor;
+    this.year_published = year;
+  }
 }
 window.onload = () => {
-    let newbooks = []
-    const showFormatedBook = () => {
-        books.map((item) => {
-            for(const clave in item ){
-                item[clave] ??= "No especifica"
-                item[clave] = item[clave].replaceAll("."," ").replaceAll(" ", "_").toLowerCase()  
-                console.log(item[clave])         
-            }
-            const book = new Book(item.title, item.autor, item.date_published)
-            newbooks.push(book)
-        } )
-    }
-    showFormatedBook() 
-    console.log("Nuevos libros", newbooks)
-}
+  let newbooks = [];
+  const bookForm = document.querySelector("#form-book");
 
+  bookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let bookValue = document.querySelector("#input-value").value;
+    searchBooks(newbooks, bookValue);
+  });
+
+  const showFormatedBook = () => {
+    books.map((item) => {
+      for (const clave in item) {
+        item[clave] ??= "No especifica";
+        item[clave] = item[clave]
+          .replaceAll(".", " ")
+          .replaceAll(" ", "_")
+          .toLowerCase();
+      }
+      const book = new Book(item.title, item.autor, item.date_published);
+      newbooks.push(book);
+    });
+  };
+  showFormatedBook();
+
+  const showFoundBook = (bookFiltered) => {
+    let showBookUI = document.querySelector("#book-filtered");
+    showBookUI.innerHTML = `
+        <div style="font-weight: bold;margin: 20px">Título: ${bookFiltered[0].title_book.replaceAll(
+          "_",
+          " "
+        )}</div>
+        <div style="font-weight: bold;margin: 20px">Autor: ${bookFiltered[0].autor_book.replaceAll(
+          "_",
+          " "
+        )}</div>
+        <div style="font-weight: bold;margin: 20px">Fecha de publicación: ${bookFiltered[0].year_published.replaceAll(
+          "_",
+          " "
+        )}</div>
+        `;
+  };
+
+  const searchBooks = (booksList, bookElected) => {
+    const bookUserFormated = bookElected
+      .replaceAll(".", " ")
+      .replaceAll(" ", "_")
+      .toLowerCase();
+    const bookFiltered = booksList.filter((book) => {
+      for (const clave in book) {
+        if (book[clave] === bookUserFormated) {
+          return book;
+        }
+      }
+    });
+    showFoundBook(bookFiltered);
+  };
+};
