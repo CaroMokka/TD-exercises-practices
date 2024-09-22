1abe770b613956d8605e1e34b496fb813bad82ce
carolina <ci.arayagonzalez@gmail.com>
2024-09-22 13:23:12 -0300

    fix: object instance is changed within a condition in the proxy

--- rebounce/M3-S8-ReservaTuHora/script.js
+++ rebounce/M3-S8-ReservaTuHora/script.js
@@ -1,10 +1,10 @@
 class Reserva {
   constructor(name, lastName, email, age, date) {
     this.name = name;
-    this.last_name = lastName
-    this.email = email
-    this.age = age
-    this.date = date
+    this.last_name = lastName;
+    this.email = email;
+    this.age = age;
+    this.date = date;
   }
 }
 window.onload = () => {
@@ -15,14 +15,17 @@ window.onload = () => {
     let user_lastName = document.getElementById("txt-lastName").value;
     let user_email = document.getElementById("txt-email").value;
     let user_age = document.getElementById("txt-age").value;
-    let user_date_hour = document.getElementById("txt-date").value; //solo me trae la fecha
-   
+    let user_date_hour = document.getElementById("txt-date").value; 
 
-    const reserva = new Reserva(user_name, user_lastName, user_email, user_age, user_date_hour);
-    console.log("reserva", reserva);
+    const reserva = {
+      name: user_name,
+      lastName: user_lastName,
+      email: user_email,
+      age: user_age,
+      date: user_date_hour,
+    };
     wrapperProxy(reserva);
   });
-  
 
   const wrapperProxy = (reserva) => {
     const proxy = new Proxy(reserva, {
@@ -31,9 +34,19 @@ window.onload = () => {
           return `La propiedad "${prop}" no existe en el objeto.`;
         if (prop === "age") {
           if (target[prop] > 18) {
+            const reservaHora = new Reserva(
+                reserva.name,
+                reserva.lastName,
+                reserva.email,
+                reserva.age,
+                reserva.date
+              );
+              console.log("Reserva Final", reservaHora);
             return `Usted tiene ${target[prop]} años, es mayor de edad.`;
           } else {
-            alert(`Usted tiene ${target[prop]} años, es menor de edad.`);
+            alert(
+              `Usted tiene ${target[prop]} años, es menor de edad. No puede reservar hora`
+            );
             return target[prop];
           }
         }
@@ -44,7 +57,6 @@ window.onload = () => {
     console.log(proxy.work);
     console.log(proxy.age);
   };
- 
 
   //falta comprender Proxy en JS
 };
